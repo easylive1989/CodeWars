4219c1ec23e98060195e03c0ec0d144cef6be95a
Paul <easylive1989@gmail.com>
2020-08-29 22:05:11 +0800

    add Backspaces in string

---
+++ Kata/BackspacesString.cs
@@ -0,0 +1,29 @@
+using System.Text;
+
+namespace Kata
+{
+    //https://www.codewars.com/kata/5727bb0fe81185ae62000ae3/train/csharp
+    public class BackspacesString
+    {
+        public string CleanString(string input)
+        {
+            var cleanString = new StringBuilder();
+            foreach (var character in input.ToCharArray())
+            {
+                if (character == '#')
+                {
+                    if (cleanString.Length > 0)
+                    {
+                        cleanString.Remove(cleanString.Length - 1, 1);
+                    }
+                }
+                else
+                {
+                    cleanString.Append(character);
+                }
+            }
+
+            return cleanString.ToString();
+        }
+    }
+}
